4c5871f3f799ac471d842348c77eae1cdd2d13b4
karanphadnisnaik <Karanphadnis1@gmail.com>
2017-08-20 12:46:10 -0700

    added same tree

---
+++ 100.SameTree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        # if both nodes are none, return True
+        if not p and not q:
+            return True
+        # if both nodes exist and share the same value, recursively call right and left subtrees
+        # else false
+        if p and q and p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        else:
+            return False
+        
